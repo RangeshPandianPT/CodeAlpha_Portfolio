4a5b317262ee7088dadeb80ee74b91b6289e2f3a
RangeshPandian PT <rangeshpandian@gmail.com>
2025-06-12 10:48:25 +0530

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,3 @@
+// JavaScript from earlier here
+function toggleTheme() { document.documentElement.toggleAttribute('data-theme', 'dark'); }
+function scrollToTop() { window.scrollTo({ top: 0, behavior: 'smooth' }); }
